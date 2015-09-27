4ab3862432f1bfe469053292e05d5f788cafb873
borislav nikolov <jack@sofialondonmoskva.com>
2015-09-27 22:28:12 +0200

    use one more hash function for NotSure, reduced the false positives to 4% and the size from 2^24 to 2^23

--- src/main/panda/NotSure.java
+++ src/main/panda/NotSure.java
@@ -1,21 +1,26 @@
 package panda;
 import java.util.BitSet;
 public class NotSure {
-    // works quite ok for 1m random keys, it gave false positive about 5% of the cases
-    // since i intend to use this for about 500k keys, 2mb bitset is good enough
-    public static final int bitset_size = 1 << 24;
+    // works quite ok for 1m random keys, it gave false positive about 4% of the cases
+    // TODO: can be greatly improved by adding more hash functions, and maybe bucketing the bitsets
+    //  (went down from 5% with 2^24bits to 4% with 2^23bits, just by using 1 more hash function
+    //   not sure if we should use more hash functions, considering http://hur.st/bloomfilter?n=1000000&p=0.04
+    //   if we go to 5 hash functions it will use almost 6699669 bits anyway (almost 2^23))
+    public static final int bitset_size = 1 << 23;
     public static final int mask = bitset_size - 1;
     public BitSet data = new BitSet(bitset_size);
-    public void clear() {
-        data.clear();
+
+    public static int l_32(long l) {
+        return (int) (l ^ (l >>> 32)); // Long.hashCode()
     }
     public boolean maybe(long l) {
-        return data.get(Murmur3.hashLong(l) & mask);
+        return data.get(l_32(l) & mask) && data.get(Murmur3.hashLong(l) & mask);
     }
     public void add(long l) {
         data.set(Murmur3.hashLong(l) & mask);
+        data.set(l_32(l) & mask);
     }
-    public String toString() {
-        return data.toString();
+    public void clear() {
+        data.clear();
     }
 }
